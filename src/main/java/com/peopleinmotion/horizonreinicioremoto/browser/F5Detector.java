35f746903a46a1d637bc191600ddcb075ebcc737
avbravo <avbravo@avbravo>
2021-12-17 21:08:55 -0500

    release-21.12.17

--- src/main/java/com/peopleinmotion/horizonreinicioremoto/browser/F5Detector.java
+++ src/main/java/com/peopleinmotion/horizonreinicioremoto/browser/F5Detector.java
@@ -3,20 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.peopleinmotion.horizonreinicioremoto.security;
+package com.peopleinmotion.horizonreinicioremoto.browser;
 
+import javax.enterprise.context.Dependent;
 import com.peopleinmotion.horizonreinicioremoto.utils.JsfUtil;
 import javax.inject.Named;
-import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
 import javax.faces.component.UIViewRoot;
 import javax.faces.context.FacesContext;
-
 /**
  *
  * @author avbravo
  */
-@Named(value = "f5Detector")
+@Named
 @SessionScoped
 public class F5Detector implements Serializable {
 
@@ -29,20 +29,21 @@ public class F5Detector implements Serializable {
 
     public void checkF5() {
         try {
-            // System.out.println("Test-->  llego a F%");
+           System.out.println("Test-->  llego a F%");
              String msg = "";
         UIViewRoot viewRoot = FacesContext.getCurrentInstance().getViewRoot();
         String id = viewRoot.getViewId();
         if (previousPage != null && (previousPage.equals(id))) { 
 // It's a reload event } previousPage = id; }
-// System.out.println("Test---> It's a reload event } previousPage = id; }");
+ System.out.println("Test---> It's a reload event } previousPage = id; }");
         }else{
-            // System.out.println("Test--> f5 else id "+id);
+            System.out.println("Test--> f5 else id "+id);
         }
         } catch (Exception e) {
-            // System.out.println("Test--> catchF5 "+e.getLocalizedMessage());
+           System.out.println("Test--> catchF5 "+e.getLocalizedMessage());
             JsfUtil.errorMessage(JsfUtil.nameOfMethod()+ " "+e.getLocalizedMessage());
         }
        
     }
+    
 }
