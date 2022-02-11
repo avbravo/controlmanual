3fe038d73e7e750fbdebe9f910e662586c0fb1be
avbravo <avbravo@avbravo>
2022-02-11 15:12:22 -0500

    con logo

--- src/main/java/com/peopleinmotion/horizonreinicioremoto/controller/BajarPlantillaController.java
+++ src/main/java/com/peopleinmotion/horizonreinicioremoto/controller/BajarPlantillaController.java
@@ -388,7 +388,7 @@ public class BajarPlantillaController implements Serializable, Page {
                 } else {
                       agendaHistorialServices.createHistorial(agendaOptional.get(), "BAJAR PLANTILLA PROGRAMAR EVENTO", user);
 
-                        AccionReciente accionReciente = accionRecienteServices.create(agendaOptional.get(), bank, cajero, accion, grupoAccion, estado,"SI","CONTROLMANUAL");
+                        AccionReciente accionReciente = accionRecienteServices.create(agendaOptional.get(), bank, cajero, accion, grupoAccion, estado,"SI","CM");
                         JmoordbContext.put("accionReciente", accionReciente);
                         /**
                          * Envio de email
